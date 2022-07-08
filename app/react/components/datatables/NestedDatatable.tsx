009652ab0d6358b53e8e5ede18eeca18f6ba887b
Chaim Lev-Ari <chiptus@gmail.com>
2022-07-08 17:46:12 +0300

    feat(ui/datatables): create nested datatable component

---
+++ app/react/components/datatables/NestedDatatable.tsx
@@ -0,0 +1,149 @@
+import {
+  useTable,
+  useFilters,
+  useSortBy,
+  Column,
+  TableState,
+} from 'react-table';
+
+import { PaginationTableSettings, SortableTableSettings } from './types';
+import { Table } from './Table';
+import { multiple } from './filter-types';
+import { TableSettingsProvider } from './useTableSettings';
+import { NestedTable } from './NestedTable';
+
+interface DefaultTableSettings
+  extends SortableTableSettings,
+    PaginationTableSettings {}
+
+interface Props<
+  D extends Record<string, unknown>,
+  TSettings extends DefaultTableSettings
+> {
+  dataset: D[];
+  columns: readonly Column<D>[];
+
+  settingsStore: TSettings;
+  getRowId?(row: D): string;
+  emptyContentLabel?: string;
+  initialTableState?: Partial<TableState<D>>;
+  isLoading?: boolean;
+}
+
+export function NestedDatatable<
+  D extends Record<string, unknown>,
+  TSettings extends DefaultTableSettings
+>({
+  columns,
+  dataset,
+  settingsStore,
+  getRowId = defaultGetRowId,
+  emptyContentLabel,
+  initialTableState = {},
+  isLoading,
+}: Props<D, TSettings>) {
+  const tableInstance = useTable<D>(
+    {
+      defaultCanFilter: false,
+      columns,
+      data: dataset,
+      filterTypes: { multiple },
+      initialState: {
+        sortBy: [settingsStore.sortBy],
+        ...initialTableState,
+      },
+      autoResetSelectedRows: false,
+      getRowId,
+      stateReducer: (newState, action) => {
+        switch (action.type) {
+          case 'toggleSortBy':
+            settingsStore.setSortBy(action.columnId, action.desc);
+            break;
+
+          default:
+            break;
+        }
+        return newState;
+      },
+    },
+    useFilters,
+    useSortBy
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  const tableProps = getTableProps();
+  const tbodyProps = getTableBodyProps();
+
+  return (
+    <NestedTable>
+      <TableSettingsProvider settings={settingsStore}>
+        <Table.Container>
+          <Table
+            className={tableProps.className}
+            role={tableProps.role}
+            style={tableProps.style}
+          >
+            <thead>
+              {headerGroups.map((headerGroup) => {
+                const { key, className, role, style } =
+                  headerGroup.getHeaderGroupProps();
+                return (
+                  <Table.HeaderRow<D>
+                    key={key}
+                    className={className}
+                    role={role}
+                    style={style}
+                    headers={headerGroup.headers}
+                  />
+                );
+              })}
+            </thead>
+            <tbody
+              className={tbodyProps.className}
+              role={tbodyProps.role}
+              style={tbodyProps.style}
+            >
+              <Table.Content<D>
+                rows={rows}
+                isLoading={isLoading}
+                prepareRow={prepareRow}
+                emptyContent={emptyContentLabel}
+                renderRow={(row, { key, className, role, style }) => (
+                  <Table.Row<D>
+                    cells={row.cells}
+                    key={key}
+                    className={className}
+                    role={role}
+                    style={style}
+                  />
+                )}
+              />
+            </tbody>
+          </Table>
+        </Table.Container>
+      </TableSettingsProvider>
+    </NestedTable>
+  );
+}
+
+function defaultGetRowId<D extends Record<string, unknown>>(row: D): string {
+  if (row.id && (typeof row.id === 'string' || typeof row.id === 'number')) {
+    return row.id.toString();
+  }
+
+  if (row.Id && (typeof row.Id === 'string' || typeof row.Id === 'number')) {
+    return row.Id.toString();
+  }
+
+  if (row.ID && (typeof row.ID === 'string' || typeof row.ID === 'number')) {
+    return row.ID.toString();
+  }
+
+  return '';
+}
+
+function emptyPlugin() {}
+
+emptyPlugin.pluginName = 'emptyPlugin';
