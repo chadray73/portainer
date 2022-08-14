84aa0253f28f850c5aa3f6992772fb980b97192f
Chaim Lev-Ari <chiptus@gmail.com>
2022-08-14 11:51:09 +0300

    refactor(edge/fdo): use datatable component

---
+++ app/portainer/settings/edge-compute/FDOProfilesDatatable/table-store.tsx
@@ -0,0 +1,26 @@
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+
+import { keyBuilder } from '@/portainer/hooks/useLocalStorage';
+
+import { paginationSettings, sortableSettings } from '@@/datatables/types';
+
+import { TableSettings } from './types';
+
+/**
+ * use for default nested table store
+ */
+
+export function createStoreHook(storageKey: string) {
+  return create<TableSettings>()(
+    persist(
+      (set) => ({
+        ...sortableSettings(set, 'state'),
+        ...paginationSettings(set),
+      }),
+      {
+        name: keyBuilder(storageKey),
+      }
+    )
+  );
+}
