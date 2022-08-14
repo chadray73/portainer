bf948ca892791da9edeea333ebde6e42d8b25829
Chaim Lev-Ari <chiptus@gmail.com>
2022-08-14 06:02:49 +0300

    refactor(edge): use datatable component for amt devices

--- app/edge/EdgeDevices/EdgeDevicesView/AMTDevicesDatatable/AMTDevicesDatatable.tsx
+++ app/edge/EdgeDevices/EdgeDevicesView/AMTDevicesDatatable/AMTDevicesDatatable.tsx
@@ -1,101 +1,35 @@
-import { usePagination, useTable } from 'react-table';
-
-import { Device } from '@/portainer/hostmanagement/open-amt/model';
-import { useAMTDevices } from '@/edge/EdgeDevices/EdgeDevicesView/AMTDevicesDatatable/useAMTDevices';
-import { RowProvider } from '@/edge/EdgeDevices/EdgeDevicesView/AMTDevicesDatatable/columns/RowContext';
 import { EnvironmentId } from '@/portainer/environments/types';
 import PortainerError from '@/portainer/error';
 
-import { NestedTable } from '@@/datatables/NestedTable';
-import { Table, TableContainer, TableHeaderRow, TableRow } from '@@/datatables';
+import { NestedDatatable } from '@@/datatables/NestedDatatable';
+import { createNestedDatatableStoreHook } from '@@/datatables/nested-table-store';
 
-import { useColumns } from './columns';
+import { useAMTDevices } from './useAMTDevices';
+import { columns } from './columns';
 
 export interface AMTDevicesTableProps {
   environmentId: EnvironmentId;
 }
 
+const useStore = createNestedDatatableStoreHook('hostname');
+
 export function AMTDevicesDatatable({ environmentId }: AMTDevicesTableProps) {
-  const columns = useColumns();
+  const store = useStore();
 
-  const { isLoading, devices, error } = useAMTDevices(environmentId);
-
-  const { getTableProps, getTableBodyProps, headerGroups, page, prepareRow } =
-    useTable<Device>(
-      {
-        columns,
-        data: devices,
-      },
-      usePagination
-    );
-
-  const tableProps = getTableProps();
-  const tbodyProps = getTableBodyProps();
+  const devicesQuery = useAMTDevices(environmentId);
 
   return (
-    <NestedTable>
-      <TableContainer>
-        <Table
-          className={tableProps.className}
-          role={tableProps.role}
-          style={tableProps.style}
-        >
-          <thead>
-            {headerGroups.map((headerGroup) => {
-              const { key, className, role, style } =
-                headerGroup.getHeaderGroupProps();
-              return (
-                <TableHeaderRow<Device>
-                  key={key}
-                  className={className}
-                  role={role}
-                  style={style}
-                  headers={headerGroup.headers}
-                />
-              );
-            })}
-          </thead>
-          <tbody
-            className={tbodyProps.className}
-            role={tbodyProps.role}
-            style={tbodyProps.style}
-          >
-            {!isLoading && devices && devices.length > 0 ? (
-              page.map((row) => {
-                prepareRow(row);
-                const { key, className, role, style } = row.getRowProps();
-
-                return (
-                  <RowProvider key={key} environmentId={environmentId}>
-                    <TableRow<Device>
-                      cells={row.cells}
-                      key={key}
-                      className={className}
-                      role={role}
-                      style={style}
-                    />
-                  </RowProvider>
-                );
-              })
-            ) : (
-              <tr>
-                <td colSpan={5} className="text-center text-muted">
-                  {userMessage(isLoading, error)}
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </Table>
-      </TableContainer>
-    </NestedTable>
+    <NestedDatatable
+      columns={columns}
+      dataset={devicesQuery.devices}
+      isLoading={devicesQuery.isLoading}
+      settingsStore={store}
+      emptyContentLabel={userMessage(devicesQuery.error)}
+    />
   );
 }
 
-function userMessage(isLoading: boolean, error?: PortainerError) {
-  if (isLoading) {
-    return 'Loading...';
-  }
-
+function userMessage(error?: PortainerError) {
   if (error) {
     return error.message;
   }
